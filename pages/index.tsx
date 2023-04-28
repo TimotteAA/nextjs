b1f908d6051366d30c77f6b5309988a517be6095
timotte <azurlane202018@gmail.com>
2023-04-28 13:23:40 +0800

    nextjs starter toolkit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/index.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <button className={styles.title}>tw测试</button>
+    </main>
+  );
+}
